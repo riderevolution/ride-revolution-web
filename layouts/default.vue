33c2568cfbf029eea96629eaa00987a2bb07a7b9
BiLos <bstamariadesignblue@gmail.com>
2019-12-02 15:44:09 +0800

    done setuping the css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
+    <div id="main_container">
         <nuxt />
+        <transition name="fade">
+            <loading v-if="$store.state.isLoading" />
+        </transition>
     </div>
 </template>
+<script>
+    import Loading from '../components/modals/Loading'
+    export default {
+        components: {
+            Loading
+        }
+    }
+</script>
