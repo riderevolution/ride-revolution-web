b8a56fd080244d4decd257129684945d8d457e31
BiLos <bstamariadesignblue@gmail.com>
2019-12-04 13:25:34 +0800

    done on the above the fold

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,23 @@
             <div class="overlay_top">
                 <img class="overlay_image" src="/stronger-together.svg" alt="ride-revolution" />
                 <div class="overlay_child">
-                    <svg id="play_icon" xmlns="http://www.w3.org/2000/svg" width="85.499" height="85.5" viewBox="0 0 85.499 85.5">
-                        <g transform="translate(0.5 0.5)">
-                            <path class="play" d="M806.695,422.713,787.279,411.3v22.829Z" transform="translate(-752.454 -380.462)" />
-                            <path class="border" d="M819.75,455.351a42.252,42.252,0,1,1-51.864-66.715" transform="translate(-751.456 -379.831)" />
-                            <path class="border" d="M834.187,441.3a42.306,42.306,0,0,1-6.325,9.807" transform="translate(-753.584 -381.298)" />
-                            <path class="border" d="M778.942,382.525a42.283,42.283,0,0,1,56.253,50.6" transform="translate(-752.222 -379.579)" />
+                    <svg id="play_icon" xmlns="http://www.w3.org/2000/svg" width="94" height="93" viewBox="0 0 94 93">
+                        <g transform="translate(-864 -325)">
+                            <g transform="translate(868 329)">
+                                <path class="play" d="M806.695,422.713,787.279,411.3v22.829Z" transform="translate(-752.454 -380.462)" />
+                                <path class="border" d="M819.75,455.351a42.252,42.252,0,1,1-51.864-66.715" transform="translate(-751.456 -379.831)" />
+                                <path class="border" d="M834.187,441.3a42.306,42.306,0,0,1-6.325,9.807" transform="translate(-753.584 -381.298)" />
+                                <path class="border" d="M778.942,382.525a42.283,42.283,0,0,1,56.253,50.6" transform="translate(-752.222 -379.579)" />
+                            </g>
+                            <rect class="rect" width="94" height="93" transform="translate(864 325)" />
                         </g>
                     </svg>
                     <p class="label">Play Video</p>
                 </div>
+            </div>
+            <div class="overlay_bottom">
+                <p>The revolution begins with you. Joir our high intensity, low impact, full body workout on a bike, led by passionate and charismatic instructors.</p>
+                <a href="javascript:void(0)" class="default_btn">Let's Begin</a>
             </div>
         </section>
     </div>
