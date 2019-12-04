9a4c489c4455624b14d8d9f6e3292a0fce0bcf5b
BiLos <bstamariadesignblue@gmail.com>
2019-12-04 12:02:09 +0800

    done with the hover overlay top

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
     <div class="home">
         <section id="banner">
-            <img src="/home-banner.png" alt="ride-revolution" />
+            <img class="main_image" src="/home-banner.jpg" alt="ride-revolution" />
+            <div class="overlay_top">
+                <img class="overlay_image" src="/stronger-together.svg" alt="ride-revolution" />
+                <div class="overlay_child">
+                    <svg id="play_icon" xmlns="http://www.w3.org/2000/svg" width="85.499" height="85.5" viewBox="0 0 85.499 85.5">
+                        <g transform="translate(0.5 0.5)">
+                            <path class="play" d="M806.695,422.713,787.279,411.3v22.829Z" transform="translate(-752.454 -380.462)" />
+                            <path class="border" d="M819.75,455.351a42.252,42.252,0,1,1-51.864-66.715" transform="translate(-751.456 -379.831)" />
+                            <path class="border" d="M834.187,441.3a42.306,42.306,0,0,1-6.325,9.807" transform="translate(-753.584 -381.298)" />
+                            <path class="border" d="M778.942,382.525a42.283,42.283,0,0,1,56.253,50.6" transform="translate(-752.222 -379.579)" />
+                        </g>
+                    </svg>
+                    <p class="label">Play Video</p>
+                </div>
+            </div>
         </section>
     </div>
 </template>
