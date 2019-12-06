2142d62b9e3d1e2b2d6d97b249ad188e38606cc9
BiLos <bstamariadesignblue@gmail.com>
2019-12-06 17:58:32 +0800

    done in the homepage

---
+++ components/Foot.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="footer">
+        <div class="logo">
+            <img src="/footer-logo.svg" />
+        </div>
+        <div class="footer_link">
+            <h3>In Studio</h3>
+            <ul>
+                <li><a href="javascript:void(0)" class="footer_item">Rider's Guide</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Buy Credits</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Class Schedules</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Instructors</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Studio</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Ride Rewards</a></li>
+            </ul>
+        </div>
+        <div class="footer_link">
+            <h3>Services</h3>
+            <ul>
+                <li><a href="javascript:void(0)" class="footer_item">FAQ's</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Contact Us</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">News</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Shop</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Privacy Policy</a></li>
+                <li><a href="javascript:void(0)" class="footer_item">Terms &amp; Conditions0</a></li>
+            </ul>
+        </div>
+        <div class="footer_link alt">
+            <h3>Let's Stay in Touch</h3>
+            <div class="social_links">
+                <h4>Follow Us</h4>
+                <a href="javascript:void(0)" class="fb">fb</a>
+                <a href="javascript:void(0)" class="ig">ig</a>
+            </div>
+            <p>Email an expert at <a href="javascript:void(0)">hello@riderev.com</a></p>
+            <p>or call/text us at <a href="javascript:void(0)">(02) 798-7447</a></p>
+        </div>
+        <div class="back_to_top">
+            <img src="/icons/back-to-top-icon.svg" /> Back to Top
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,21 @@
 <template>
     <div id="main_container">
-        <transition name="fade">
-            <navbar />
-        </transition>
+        <navbar />
         <nuxt />
         <transition name="fade">
             <loading v-if="$store.state.isLoading" />
         </transition>
+        <foot />
     </div>
 </template>
 <script>
     import Navbar from '../components/Navbar'
+    import Foot from '../components/Foot'
     import Loading from '../components/modals/Loading'
     export default {
         components: {
             Navbar,
+            Foot,
             Loading
         }
     }
