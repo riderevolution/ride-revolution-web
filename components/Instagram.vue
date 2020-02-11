ca7e9bddedb399cc5ed16ebf68c3c6fa0e3a0ea0
BiLos <bstamariadesignblue@gmail.com>
2020-02-11 16:54:59 +0800

    done in the home page responsive

--- components/Instagram.vue
+++ components/Instagram.vue
@@ -1,10 +1,13 @@
 <template>
     <div id="instagram">
-        <div :class="`image_wrapper ${(lessOne) ? 'less_one' : ''}`">
-            <a href="javascript:void(0)" class="image" v-for="(n, key) in 5" :key="key" v-if="!lessOne">
+        <div :class="`image_wrapper ${(lessOne) ? 'less_one' : ($parent.$parent.$parent.isMobile ? 'mobile' : '')}`">
+            <a href="javascript:void(0)" class="image" v-for="(n, key) in 5" :key="key" v-if="!lessOne && !$parent.$parent.$parent.isMobile">
                 <img :src="`/default/lets-ride/sample-ig${key}.jpg`" />
             </a>
-            <a href="javascript:void(0)" class="image" v-for="(n, key) in 4" :key="key" v-if="lessOne">
+            <a href="javascript:void(0)" class="image" v-for="(n, key) in 4" :key="key" v-if="lessOne && !$parent.$parent.$parent.isMobile">
+                <img :src="`/default/lets-ride/sample-ig${key}.jpg`" />
+            </a>
+            <a href="javascript:void(0)" class="image" v-for="(n, key) in 3" :key="key" v-if="$parent.$parent.$parent.isMobile">
                 <img :src="`/default/lets-ride/sample-ig${key}.jpg`" />
             </a>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@
             <h1>Ride Revolution</h1>
             <img class="main_image" src="default/home/home-banner.jpg" alt="ride-revolution" />
             <div class="overlay_top">
-                <img class="overlay_image" src="default/home/stronger-together.svg" alt="ride-revolution" />
+                <img class="overlay_image" src="default/home/stronger-together.svg" alt="ride-revolution" v-if="!$parent.$parent.isMobile" />
+                <img class="overlay_image" src="default/home/home-banner-mobile.svg" alt="ride-revolution" v-else />
                 <div class="overlay_child">
                     <svg id="play_icon" xmlns="http://www.w3.org/2000/svg" width="94" height="93" viewBox="0 0 94 93">
                         <g transform="translate(-864 -325)">
@@ -23,6 +24,9 @@
             <div class="overlay_bottom">
                 <p>The revolution begins with you. Joir our high intensity, low impact, full body workout on a bike, led by passionate and charismatic instructors.</p>
                 <a href="javascript:void(0)" class="default_btn">Let's Begin</a>
+                <div class="scroll_mobile" v-if="$parent.$parent.isMobile">
+                    <img src="/icons/scroll-down.svg" @click="scrollDown()" />
+                </div>
             </div>
         </section>
         <section id="promos" @mouseenter="showAllPromos = true" @mouseleave="showAllPromos = false">
@@ -42,25 +46,28 @@
                             </div>
                         </swiper-slide>
                         <div class="swiper-pagination" slot="pagination"></div>
-                        <div class="swiper-button-prev" slot="button-prev"></div>
-                        <div class="swiper-button-next" slot="button-next"></div>
+                        <div class="swiper-button-prev" slot="button-prev" v-if="!$parent.$parent.isMobile"></div>
+                        <div class="swiper-button-next" slot="button-next" v-if="!$parent.$parent.isMobile"></div>
                     </swiper>
                     <transition name="slideX">
-                        <div class="overlay_btn default_btn" v-if="showAllPromos">See All Promos</div>
+                        <nuxt-link to="/whats-new" class="overlay_btn default_btn" v-if="showAllPromos && !$parent.$parent.isMobile">See All Promos</nuxt-link>
                     </transition>
+                    <div class="action_mobile" v-if="$parent.$parent.isMobile">
+                        <nuxt-link to="/whats-new" class="default_btn">See All Promos</nuxt-link>
+                    </div>
                 </div>
             </no-ssr>
         </section>
         <section id="packages">
             <div class="header">
-                <h2 class="image_bg">packages</h2>
+                <h2 :class="`image_bg ${($parent.$parent.isMobile) ? 'mobile' : ''}`">packages</h2>
                 <div class="description">
                     <p>Start your revolution! Purchase a class before you book a bike. You have <span class="red">30 days</span> to activate your class package.</p>
-                    <a class="link" href="javscript:void(0)"><span>See All Class Packages</span> <div></div><div></div><div></div></a>
+                    <nuxt-link class="link" to="/buy-rides" v-if="!$parent.$parent.isMobile"><span>See All Class Packages</span> <div></div><div></div><div></div></nuxt-link>
                 </div>
             </div>
-            <div class="content">
-                <a href="javascript:void(0)" :class="`package_wrapper ${(data.has_promo) ? 'promo' : ''}`" v-for="(data, key) in packages" :key="key">
+            <div class="content" v-if="!$parent.$parent.isMobile">
+                <nuxt-link :to="`/buy-rides/package/${convertToSlug(data.title)}`" :class="`package_wrapper ${(data.has_promo) ? 'promo' : ''}`" v-for="(data, key) in packages" :key="key">
                     <div class="ribbon" v-if="data.has_promo">Promo</div>
                     <div class="package_header">
                         <h2 class="title">{{ data.title }}</h2>
@@ -70,19 +77,40 @@
                     <div class="price">Php {{ totalItems(data.price) }}</div>
                     <div class="expires">{{ data.expire }}</div>
                     <div class="default_btn_out"><span>Buy Now</span></div>
-                </a>
+                </nuxt-link>
+            </div>
+            <div class="content" v-else>
+                <no-ssr>
+                    <swiper :options="promoOptions" class="default">
+                        <swiper-slide :class="`package_wrapper slider ${(data.has_promo) ? 'promo' : ''}`" v-for="(data, key) in packages" :key="key">
+                            <nuxt-link :to="`/buy-rides/package/${convertToSlug(data.title)}`">
+                                <div class="ribbon" v-if="data.has_promo">Promo</div>
+                                <div class="package_header">
+                                    <h2 class="title">{{ data.title }}</h2>
+                                    <div class="description" v-line-clamp="3" v-html="data.description"></div>
+                                </div>
+                                <div class="discounted_price" v-if="data.has_promo">Php {{ totalItems(data.discounted_price) }}</div>
+                                <div class="price">Php {{ totalItems(data.price) }}</div>
+                                <div class="expires">{{ data.expire }}</div>
+                                <div class="default_btn_out"><span>Buy Now</span></div>
+                            </nuxt-link>
+                        </swiper-slide>
+                        <div class="swiper-pagination" slot="pagination"></div>
+                    </swiper>
+                    <nuxt-link class="link_mobile" to="/buy-rides" v-if="$parent.$parent.isMobile"><span>See All Class Packages</span> <div></div><div></div><div></div></nuxt-link>
+                </no-ssr>
             </div>
         </section>
         <section id="reviews">
             <div class="header">
-                <h2 class="image_bg">reviews</h2>
+                <h2 :class="`image_bg ${($parent.$parent.isMobile) ? 'mobile' : ''}`">reviews</h2>
                 <div class="description">
                     <p><strong>We’ve heard things like…</strong></p>
                 </div>
             </div>
             <div class="content">
                 <no-ssr>
-                    <swiper :options="reviewOptions" class="default alt">
+                    <swiper :options="reviewOptions" class="default alt" v-if="!$parent.$parent.isMobile">
                         <swiper-slide class="review_slide" v-for="(data, key) in reviews" :key="key">
                             <div class="description" v-html="data.description"></div>
                             <img :src="data.path" alt="" />
@@ -90,6 +118,14 @@
                         </swiper-slide>
                         <div class="swiper-button-prev" slot="button-prev"></div>
                         <div class="swiper-button-next" slot="button-next"></div>
+                    </swiper>
+                    <swiper :options="promoOptions" class="default" v-else>
+                        <swiper-slide class="review_slide mobile" v-for="(data, key) in reviews" :key="key">
+                            <div class="description" v-html="data.description"></div>
+                            <img :src="data.path" alt="" />
+                            <h2 class="title">{{ data.title }}</h2>
+                        </swiper-slide>
+                        <div class="swiper-pagination" slot="pagination"></div>
                     </swiper>
                 </no-ssr>
             </div>
@@ -100,16 +136,22 @@
                 <div class="image">
                     <img src="/default/home/instructors-cover.png" alt="ride-revolution-instructor" />
                 </div>
-                <div class="info">
+                <div class="info" v-if="!$parent.$parent.isMobile">
                     <h2 class="title">Train With The Best</h2>
                     <div class="description">We have the most exciting instructors to allow you to lose <br> yourself, let's go and have fun.</div>
+                    <nuxt-link to="/instructors" class="default_btn">Meet our Instructors</nuxt-link>
+                </div>
+                <div class="info mobile" v-else>
+                    <img src="/default/home/instructor-mobile.svg" />
+                    <h2 class="title">Train With The Best</h2>
+                    <div class="description">We have the most exciting instructors to allow you to lose yourself, let's go and have fun.</div>
                     <nuxt-link to="/instructors" class="default_btn">Meet our Instructors</nuxt-link>
                 </div>
             </div>
         </section>
         <section id="studios">
             <div class="header">
-                <h2 class="image_bg">studios</h2>
+                <h2 :class="`image_bg ${($parent.$parent.isMobile) ? 'mobile' : ''}`">studios</h2>
                 <div class="description">
                     <p><strong>Explore Our Studios</strong></p>
                     <a href="javascript:void(0)" class="default_btn">See All</a>
@@ -128,14 +170,14 @@
                                         </select>
                                     </div>
                                 </div>
-                                <div class="content_flex">
+                                <div class="content_flex" v-if="!$parent.$parent.isMobile">
                                     <h2 class="title">{{ studio.name }}</h2>
                                     <div class="description">
                                         <label>Opening Hours</label>
                                         <div class="opening" v-html="studio.opening"></div>
                                     </div>
                                 </div>
-                                <div class="content_flex">
+                                <div class="content_flex" v-if="!$parent.$parent.isMobile">
                                     <div class="description">
                                         <label>Contact Details</label>
                                         <div class="link">
@@ -152,7 +194,7 @@
                             </div>
                         </div>
                         <div class="left">
-                            <no-ssr>
+                            <no-ssr v-if="!$parent.$parent.isMobile">
                                 <swiper :options="studioOptions" class="default">
                                     <swiper-slide class="studio_slide" v-for="(studio, key) in studio.images" :key="key">
                                         <img :src="studio.path" alt="" />
@@ -161,10 +203,44 @@
                                         </div>
                                     </swiper-slide>
                                     <div class="swiper-pagination" slot="pagination"></div>
-                                    <div class="swiper-button-prev" slot="button-prev"></div>
-                                    <div class="swiper-button-next" slot="button-next"></div>
+                                    <div class="swiper-button-prev" slot="button-prev" v-if="!$parent.$parent.isMobile"></div>
+                                    <div class="swiper-button-next" slot="button-next" v-if="!$parent.$parent.isMobile"></div>
                                 </swiper>
                             </no-ssr>
+                            <no-ssr v-else>
+                                <swiper :options="promoOptions" class="default alt3">
+                                    <swiper-slide class="studio_slide" v-for="(studio, key) in studio.images" :key="key">
+                                        <img :src="studio.path" alt="" />
+                                        <div class="overlay">
+                                            <h2 class="title">{{ studio.title }}</h2>
+                                        </div>
+                                    </swiper-slide>
+                                    <div class="swiper-pagination" slot="pagination"></div>
+                                    <div class="swiper-button-prev" slot="button-prev" v-if="!$parent.$parent.isMobile"></div>
+                                    <div class="swiper-button-next" slot="button-next" v-if="!$parent.$parent.isMobile"></div>
+                                </swiper>
+                            </no-ssr>
+                            <div class="content_flex" v-if="$parent.$parent.isMobile">
+                                <h2 class="title">{{ studio.name }}</h2>
+                                <div class="description">
+                                    <label>Opening Hours</label>
+                                    <div class="opening" v-html="studio.opening"></div>
+                                </div>
+                            </div>
+                            <div class="content_flex" v-if="$parent.$parent.isMobile">
+                                <div class="description">
+                                    <label>Contact Details</label>
+                                    <div class="link">
+                                        <img src="/icons/email-icon.svg" />
+                                        <a href="javascript:void(0)" class="email">{{ studio.mail }}</a>
+                                    </div>
+                                    <div class="link">
+                                        <img src="/icons/phone-icon.svg" />
+                                        <a href="javascript:void(0)">{{ studio.contact }}</a>
+                                    </div>
+                                </div>
+                                <a href="javascript:void(0)" class="default_btn">Explore</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -172,7 +248,7 @@
         </section>
         <section id="lets_ride">
             <div class="header">
-                <h2 class="image_bg">studios</h2>
+                <h2 :class="`image_bg ${($parent.$parent.isMobile) ? 'mobile' : ''}`">studios</h2>
                 <div class="description">
                     <img src="/icons/lets-ride-ig-icon.svg" alt="lets-ride" /><span>@riderevolution</span>
                 </div>
@@ -197,7 +273,6 @@
                 promoOptions: {
                     slidesPerView: 1,
                     spaceBetween: 30,
-                    effect: 'fade',
                     loop: true,
                     autoplay: {
                         delay: 4000,
@@ -210,6 +285,12 @@
                     navigation: {
                         nextEl: '.swiper-button-next',
                         prevEl: '.swiper-button-prev'
+                    },
+                    breakpoints: {
+                        1024: {
+                            slidesPerView: 1,
+                            spaceBetween: 0
+                        }
                     }
                 },
                 studioOptions: {
@@ -426,6 +507,12 @@
             }
         },
         methods: {
+            scrollDown () {
+                const me = this
+                me.$scrollTo('#promos', {
+                    offset: -65
+                })
+            },
             swiperEvent (type) {
                 const me = this
                 switch (type) {
