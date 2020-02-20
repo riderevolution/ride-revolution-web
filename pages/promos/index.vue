2f5d9b55d14fb58fd0bdb79711d92247e8635970
BiLos <bstamariadesignblue@gmail.com>
2020-02-20 18:34:51 +0800

    fixed samiethong

---
+++ pages/promos/index.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="buy_rides">
+        <section id="banner">
+            <img src="/default/promo/sample-image.jpg" />
+            <breadcrumb :overlay="true" />
+            <div class="overlay_mid">
+                <h1>Ride Rev Promos</h1>
+                <h2>We're revolutionizing Manila's fitness industry.</h2>
+            </div>
+        </section>
+        <section id="promos" class="alt">
+            <div class="promo_list" v-for="(data, key) in promos" :key="key">
+                <h2 class="header_title" v-line-clamp="3">Ride Rev Promo</h2>
+                <h3 class="title">{{ data.title }}</h3>
+                <div class="description" v-line-clamp="4" v-html="data.description"></div>
+                <div class="copy_wrapper" v-if="data.hasCode">
+                    <input class="code" :id="`code_${key}`" :value="data.code" />
+                    <button type="button" class="default_btn" @click="codeClipboard(data, key)">Copy Code</button>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    import Breadcrumb from '../../components/Breadcrumb'
+    export default {
+        components: {
+            Breadcrumb
+        },
+        data () {
+            return {
+                showAllPromos: false,
+                promos: [
+                    {
+                        path: '/default/promo/sample-image.jpg',
+                        title: 'Complete all 20 milestone badges to get an exclusive prize from us!',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore. tempor incididunt ut labore et dolore incididunt ut labore et',
+                        hasCode: false
+                    },
+                    {
+                        path: '/default/promo/sample-image.jpg',
+                        title: 'Get 1,500 Pesos Discount on your Ride!*',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore. sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore.',
+                        hasCode: true,
+                        code: 'ASD1231'
+                    },
+                    {
+                        path: '/default/promo/sample-image.jpg',
+                        title: 'Get 1,500 Pesos Discount on your Ride!*',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore. sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore.',
+                        hasCode: true,
+                        code: 'HGJ23A'
+                    },
+                    {
+                        path: '/default/promo/sample-image.jpg',
+                        title: 'Complete all 20 milestone badges to get an exclusive prize from us!',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore. tempor incididunt ut labore et dolore incididunt ut labore et',
+                        hasCode: false
+                    },
+                    {
+                        path: '/default/promo/sample-image.jpg',
+                        title: 'Get 1,500 Pesos Discount on your Ride!*',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore. sed do eiusmod tempor incididunt ut labore et dolore incididunt ut labore et dolore.',
+                        hasCode: true,
+                        code: 'JHSHAI23'
+                    }
+                ]
+            }
+        },
+        methods: {
+        }
+    }
+</script>
