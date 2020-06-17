e1e3943694620b2688af4f97719ea65158737c3e
teryong <dthcrpz@gmail.com>
2020-06-17 18:01:58 +0800

    paymaya

--- pages/buy-rides/store-credit/_slug.vue
+++ pages/buy-rides/store-credit/_slug.vue
@@ -78,6 +78,7 @@
                             <div class="total">
                                 <p>You Pay</p>
                                 <p>Php {{ totalCount(form.total) }}</p>
+                                <a href="javascript:void(0)" @click="paymaya()">wewew</a>
                             </div>
                             <div class="preview_actions">
                                 <div class="default_btn_blk" @click="stepBack()" v-if="!$parent.$parent.isMobile">Back</div>
@@ -175,6 +176,37 @@
             }
         },
         methods: {
+            paymaya () {
+                const me = this
+                let token = me.$cookies.get('token')
+                let formData = new FormData()
+                formData.append('type', 'store-credit')
+                formData.append('store_credit_id', me.res.id)
+                formData.append('price', me.res.amount)
+                formData.append('quantity', me.form.quantity)
+                formData.append('total', me.form.total)
+                formData.append('payment_method', 'paymaya')
+
+                me.$axios.post('api/paymaya', formData, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }).then(res => {
+                    console.log(res.data)
+                //     if (res.data) {
+                //         me.$store.state.buyRidesSuccessStatus = true
+                //     }
+                // }).catch(err => {
+                //     me.$store.state.errorList = err.response.data.errors
+                //     me.$store.state.errorPromptStatus = true
+                // }).then(() => {
+                //     me.step = 0
+                //     setTimeout( () => {
+                //         me.loader(false)
+                //     }, 500)
+                //     me.validateToken()
+                })
+            },
             computeTotal (total) {
                 const me = this
                 me.form.total = total
