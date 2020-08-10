aa002c68843c7a8f92437e05b17f005fb33705f7
BiLos <billyrosssta.maria@gmail.com>
2020-08-10 19:14:10 +0800

    fixed the responsive of the news description

--- nuxt.config.js
+++ nuxt.config.js
@@ -103,8 +103,8 @@ export default {
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
     // baseURL: (process.env.NODE_ENV == "development") ? 'http://api.riderevolution.test' : 'https://api.riderevolution.ph',
-    baseURL: (process.env.NODE_ENV == "development") ? 'https://3476cd1961a6.ngrok.io' : 'https://api.riderevolution.ph',
-    // baseURL: (process.env.NODE_ENV == "development") ? 'http://127.0.0.1:8000' : 'https://api.riderevolution.ph',
+    // baseURL: (process.env.NODE_ENV == "development") ? 'https://610968818b2e.ngrok.io' : 'https://api.riderevolution.ph',
+    baseURL: (process.env.NODE_ENV == "development") ? 'http://127.0.0.1:8000' : 'https://api.riderevolution.ph',
     credentials: false
   },
   env: {
