e2a1c7c08d4cc38d237c13822f26aedcc5a6ddf7
BiLos <bstamariadesignblue@gmail.com>
2020-08-19 15:15:16 +0800

    done in the share/transfer

--- components/modals/ShareTransferPackage.vue
+++ components/modals/ShareTransferPackage.vue
@@ -4,13 +4,13 @@
             <form id="default_form" class="overlay alt_2" @submit.prevent="submissionSuccess()">
                 <div class="modal_wrapper">
                     <h2 class="form_title alt">{{ (category == 'transfer') ? 'Transfer' : 'Share' }} Class Package</h2>
-                    <h3 class="form_subtitle">You are about to {{ category }} your <b>{{ data.class_package.name }}</b>. Please input the recipient's member ID.</h3>
+                    <h3 class="form_subtitle">You are about to {{ category }} your <b>{{ data.class_package.name }}</b>. Please input the recipient's username or email address.</h3>
                     <div class="form_close" @click="toggleClose()"></div>
                     <div class="modal_main_group">
                         <div class="form_flex with_btn alt_2">
                             <div class="form_group">
-                                <label for="receiver_id">Recipients Username <span>*</span></label>
-                                <input type="text" name="receiver_id" v-model="form.receiver_id" autocomplete="off" placeholder="Enter your recipients username" class="input_text" v-validate="{required: true, regex: '^[a-zA-Z0-9]*$'}">
+                                <label for="receiver_id">Recipient<span>*</span></label>
+                                <input type="text" name="receiver_id" v-model="form.receiver_id" autocomplete="off" placeholder="Enter recipient username or email" class="input_text" v-validate="{required: true, min: 6, regex: '^[a-zA-Z0-9/-/_|\@|\.|\#]*$'}">
                                 <transition name="slide"><span class="validation_errors" v-if="errors.has('receiver_id')">{{ properFormat(errors.first('receiver_id')) }}</span></transition>
                             </div>
                             <div class="form_button alt">
@@ -59,7 +59,7 @@
                                 formData.append('sender_id', res.data.user.id)
                                 formData.append('user_package_count_id', me.data.id)
                                 formData.append('receiver_id', me.form.receiver_id)
-                                me.$axios.post(`api/packages/class-packages/${me.category}`, formData).then(res => {
+                                me.$axios.post(`api/packages/class-packages/${me.category}?forWeb=1`, formData).then(res => {
                                     if (res.data) {
                                         setTimeout( () => {
                                             me.$store.state.shareTransferPackageStatus = false
