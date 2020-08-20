e1b3732e87d5a787b31c9ea23d822ab77eb99fd2
BiLos <bstamariadesignblue@gmail.com>
2020-08-20 15:46:05 +0800

    added copy

--- pages/thank-you/index.vue
+++ pages/thank-you/index.vue
@@ -4,8 +4,9 @@
             <div class="logo">
                 <img src="/footer-logo.svg" />
             </div>
-    		<h1 class="header_title">{{ ($store.state.oldUser) ? 'Thank you for coming back' : 'Thank you for signing up!' }}</h1>
-        	<h2 class="header_subtitle alt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h2>
+    		<h1 class="header_title">{{ ($store.state.oldUser) ? "You're all set!" : "You're all set!" }}</h1>
+        	<h2 class="header_subtitle alt" v-if="$store.state.oldUser">Your NEW password has been updated. And now it's time to start your Revolution. Let's Ride!</h2>
+        	<h2 class="header_subtitle alt" v-else>Your ACCOUNT has been created. And now it's time to start your Revolution. Let's Ride!</h2>
         	<div class="default_btn" @click="loginUser()" v-if="!$store.state.oldUser && !$store.state.newUser"><span>Click Here to Login</span></div>
         	<nuxt-link to="/my-profile" class="default_btn" v-else-if="$store.state.oldUser && !$store.state.newUser"><span>Go To My Profile</span></nuxt-link>
         	<nuxt-link to="/my-profile" class="default_btn" v-else-if="!$store.state.oldUser && $store.state.newUser"><span>Go To My Profile</span></nuxt-link>
