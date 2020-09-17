6296cabe875a456496819173f885e6dafd93e0ca
teryong <dthcrpz@gmail.com>
2020-09-17 18:32:48 +0800

    paymaya

--- pages/testing/paymaya.vue
+++ pages/testing/paymaya.vue
@@ -13,90 +13,12 @@
 		},
 		methods: {
 			initiatePaymaya () {
-				let paymayaObject = {
-				  "totalAmount": {
-				    "value": 100,
-				    "currency": "PHP",
-				    "details": {
-				      "discount": 0,
-				      "serviceCharge": 0,
-				      "shippingFee": 0,
-				      "tax": 0,
-				      "subtotal": 100
-				    }
-				  },
-				  "buyer": {
-				    "firstName": "John",
-				    "middleName": "Paul",
-				    "lastName": "Doe",
-				    "birthday": "1995-10-24",
-				    "customerSince": "1995-10-24",
-				    "sex": "M",
-				    "contact": {
-				      "phone": "+639181008888",
-				      "email": "merchant@merchantsite.com"
-				    },
-				    "shippingAddress": {
-				      "firstName": "John",
-				      "middleName": "Paul",
-				      "lastName": "Doe",
-				      "phone": "+639181008888",
-				      "email": "merchant@merchantsite.com",
-				      "line1": "6F Launchpad",
-				      "line2": "Reliance Street",
-				      "city": "Mandaluyong City",
-				      "state": "Metro Manila",
-				      "zipCode": "1552",
-				      "countryCode": "PH",
-				      "shippingType": "ST" // ST - for standard, SD - for same day
-				    },
-				    "billingAddress": {
-				      "line1": "6F Launchpad",
-				      "line2": "Reliance Street",
-				      "city": "Mandaluyong City",
-				      "state": "Metro Manila",
-				      "zipCode": "1552",
-				      "countryCode": "PH"
-				    }
-				  },
-				  "items": [
-				    {
-				      "name": "Canvas Slip Ons",
-				      "quantity": 1,
-				      "code": "CVG-096732",
-				      "description": "Shoes",
-				      "amount": {
-				        "value": 100,
-				        "details": {
-				          "discount": 0,
-				          "serviceCharge": 0,
-				          "shippingFee": 0,
-				          "tax": 0,
-				          "subtotal": 100
-				        }
-				      },
-				      "totalAmount": {
-				        "value": 100,
-				        "details": {
-				          "discount": 0,
-				          "serviceCharge": 0,
-				          "shippingFee": 0,
-				          "tax": 0,
-				          "subtotal": 100
-				        }
-				      }
-				    }
-				  ],
-				  "redirectUrl": {
-				    "success": "https://www.merchantsite.com/success",
-				    "failure": "https://www.merchantsite.com/failure",
-				    "cancel": "https://www.merchantsite.com/cancel"
-				  },
-				  "requestReferenceNumber": "1551191039",
-				  "metadata": {}
-				}
+				let targetHTMLElement = document.querySelector('.paymaya-checkout')
+
 				PayMayaSDK.init('pk-3yJqfuy3fKZLcLSG9ksmpH4rYsPrHVk9fURYWLVGiLq', true)
-				PayMayaSDK.createCheckout(paymayaObject)
+				PayMayaSDK.createCreditCardForm(targetHTMLElement, {
+					buttonText: 'Submit'
+				})
 			}
 		},
 		mounted () {
@@ -104,3 +26,8 @@
 		}
 	}
 </script>
+
+<style lang="stylus">
+	.paymaya-checkout 
+		width: 100%
+</style>
