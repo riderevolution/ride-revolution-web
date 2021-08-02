64e193713b2d6df5b33a98d9c24bc1fd88e96580
teryong <dthrcrpz@gmail.com>
2021-08-02 15:34:52 +0800

    added gcash on digital gift card purchases

--- pages/buy-rides/digital-gift-card/index.vue
+++ pages/buy-rides/digital-gift-card/index.vue
@@ -182,6 +182,8 @@
                                 <div class="right">
                                     <div :class="`default_btn_blue ${(parseInt(storeCredits) <= parseInt((promoApplied) ? selectedPackage.final_price : (selectedPackage.is_promo == 1 ? selectedPackage.discounted_price : selectedPackage.package_price))) ? 'disabled' : ''}`" v-if="type == 'store-credits'" @click="paymentSuccess()">Pay Now</div>
                                     <div class="default_btn_blue" @click="paymaya()" v-if="type == 'paynow'">Debit/Credit Card</div>
+                                    <br><br>
+                                    <div class="default_btn_blue" @click="gcash()" v-if="type == 'paynow'">GCash</div>
                                     <div id="paypal-button-container" v-if="type == 'paynow'"></div>
                                 </div>
                             </div>
@@ -200,6 +202,8 @@
                                 <div class="right">
                                     <div :class="`default_btn_blue ${(parseInt(storeCredits) <= parseInt((promoApplied) ? selectedPackage.final_price : (selectedPackage.is_promo == 1 ? selectedPackage.discounted_price : selectedPackage.package_price))) ? 'disabled' : ''}`" v-if="type == 'store-credits'" @click="paymentSuccess()">Pay Now</div>
                                     <div class="default_btn_blue" @click="paymaya()" v-if="type == 'paynow'">Debit/Credit Card</div>
+                                    <br><br>
+                                    <div class="default_btn_blue" @click="gcash()" v-if="type == 'paynow'">GCash</div>
                                     <div id="paypal-button-container" v-if="type == 'paynow'"></div>
                                     <div class="paypal_disclaimer" v-if="type == 'paynow'">
                                         <p>Note: Paypal account not needed</p>
@@ -292,10 +296,33 @@
                 res: [],
                 classPackages: [],
                 predefinedTitles: [],
-                selectedPackage: null
+                selectedPackage: null,
+                paymongoData: null
             }
         },
         methods: {
+            gcash () {
+                const me = this
+                let token = me.$cookies.get('70hokc3hhhn5')
+                me.loader(true)
+                me.form.url = location.href
+                me.$axios.post(`api/paymongo/sources`, me.form, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }).then(res => {
+                    me.paymongoData = res.data.source.data
+                    me.payment(me, null, 'digital-gift-card', 0, null, true)
+                }).catch(err => {
+                    me.$store.state.loginSignUpStatus = true
+                    document.body.classList.add('no_scroll')
+                    me.$nuxt.error({ statusCode: 403, message: 'Something Went Wrong' })
+                }).then(() => {
+                    setTimeout( () => {
+                        me.loader(false)
+                    }, 500)
+                })
+            },
             paymaya () {
                 const me = this
                 let token = me.$cookies.get('70hokc3hhhn5')
