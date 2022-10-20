6bf65ce95287fc9ddb5c5e8f9c193c2dce13de7b
BiLosxD <billyrosssta.maria@gmail.com>
2022-10-20 10:48:01 +0800

    done adjusting the button for home page `AboveTheFold`

--- components/home/AboveTheFold.vue
+++ components/home/AboveTheFold.vue
@@ -11,7 +11,7 @@
             <template v-if="above_the_fold.subtitle">
                 <p class="nmr" v-html="above_the_fold.subtitle"></p>
             </template>
-            <div :class="[ 'overlay_flex', (above_the_fold.subtitle) ? '' : 'end' ]">
+            <div :class="[ 'overlay_flex', (above_the_fold.subtitle) ? '' : 'center' ]">
                 <a :href="above_the_fold.button_link" rel="canonical" class="default_btn">{{ above_the_fold.button_text }}</a>
                 <template v-if="above_the_fold.show_play_video">
                     <div class="default_btn_wht" @click="toggleVideo()">Play Video</div>
@@ -24,7 +24,7 @@
                 <div class="overlay_child" v-if="!$store.state.isMobile" @click="toggleVideo()"> <svg id="play_icon" xmlns="http://www.w3.org/2000/svg" width="94" height="93" viewBox="0 0 94 93"> <g transform="translate(-864 -325)"> <g transform="translate(868 329)"> <path class="play" d="M806.695,422.713,787.279,411.3v22.829Z" transform="translate(-752.454 -380.462)" /> <path class="border" d="M819.75,455.351a42.252,42.252,0,1,1-51.864-66.715" transform="translate(-751.456 -379.831)" /> <path class="border" d="M834.187,441.3a42.306,42.306,0,0,1-6.325,9.807" transform="translate(-753.584 -381.298)" /> <path class="border" d="M778.942,382.525a42.283,42.283,0,0,1,56.253,50.6" transform="translate(-752.222 -379.579)" /> </g> <rect class="rect" width="94" height="93" transform="translate(864 325)" /> </g> </svg> <div class="label">Play Video</div> </div>
             </template>
         </div>
-        <div :class="[ 'overlay_bottom', (above_the_fold.subtitle) ? '' : 'end' ]" v-if="!$store.state.isMobile">
+        <div :class="[ 'overlay_bottom', (above_the_fold.subtitle) ? '' : 'center' ]" v-if="!$store.state.isMobile">
             <template v-if="above_the_fold.subtitle">
                 <p class="nmr" v-html="above_the_fold.subtitle"></p>
             </template>
